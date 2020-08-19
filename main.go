a34add7de94a95f09a597c52e0f224b0392cbe55
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-08-19 15:56:02 +0900

    Fix connect string

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	db, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	db, err := ent.Open("sqlite3", "user.db?_fk=1")
 	if err != nil {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
